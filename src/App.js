761c77a0eafc415775e90e715f2002a7199d1d84
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-01-03 11:46:38 +0530

    feat: add Landing Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { LandingPage } from "./pages";
 
 function App() {
-  return <div className="App">This is the main page</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
