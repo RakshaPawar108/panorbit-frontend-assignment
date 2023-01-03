7ca66339ea60b79441dc503a3e310bc6883359cd
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-01-03 12:31:55 +0530

    feat: add utility function for api call

--- src/context/users-context.jsx
+++ src/context/users-context.jsx
@@ -1,9 +1,15 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { getUsers } from "../utils";
 
 const UsersContext = createContext(null);
 
 const UsersProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers(setUsers);
+  }, []);
+
   return (
     <UsersContext.Provider value={users}>{children}</UsersContext.Provider>
   );
---
+++ src/utils/getUsers.js
@@ -0,0 +1,12 @@
+import { fetchUsers } from "../services/Users/fetchUsers";
+
+export const getUsers = async (setUsers) => {
+  try {
+    const response = await fetchUsers();
+    if (response.status === 200) {
+      setUsers(response.data.users);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
