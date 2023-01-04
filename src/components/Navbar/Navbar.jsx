33290e46aff2e76ef54a0ff3a215e25b5e27b179
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-01-04 17:52:01 +0530

    feat: add side navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+import { Container } from "react-bootstrap";
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return <Container fluid className="nav"></Container>;
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { UserListItem } from "./UserListItem/UserListItem";
 export { UserList } from "./UserList/UserList";
+export { Navbar } from "./Navbar/Navbar";
